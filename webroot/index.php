d4c94d5f01419c7e834737411e7321e0db8366d2
Nick Baker <nick@webtechnick.com>
2013-08-29 11:37:08 -0600

    updating core

--- webroot/index.php
+++ webroot/index.php
@@ -54,7 +54,7 @@ if (!defined('APP_DIR')) {
  * The following line differs from its sibling
  * /app/webroot/index.php
  */
-define('CAKE_CORE_INCLUDE_PATH',  APP . 'Vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . APP_DIR . DS . 'Vendor' . DS . 'pear-pear.cakephp.org' . DS . 'CakePHP');
 
 /**
  * Editing below this line should NOT be necessary.
